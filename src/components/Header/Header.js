138a3864f54123015ffce5f124a9b65be90e3ed2
KobinaOtc <kobinaotchereadjoku@gmail.com>
2017-11-23 13:25:37 +0000

    Made header and skill components more responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ let Header = (props) => (
                   <h1 className="mt-5">{props.name}</h1>
                   <h3>{props.description}</h3>
                 </div>
-                <div className="col-lg-4 mt-5 d-none d-lg-block">
+                <div className="col-lg-4 my-5 d-none d-lg-block">
                   <img src="https://z-p3-scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/10407976_928437500504430_408290674394934566_n.jpg?oh=c8265c17ce5f217fcd67b9755fe96695&oe=5A94CA4D" className="img-border img-fluid"/>
                 </div>
               </div>
